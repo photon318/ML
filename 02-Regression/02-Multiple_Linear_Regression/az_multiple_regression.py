c3b094fa05b033710a0c220177c4b5bfaa4a9720
photon318 <photon318@gmail.com>
2017-09-25 22:40:37 +0300

    white space

--- 02-Regression/02-Multiple_Linear_Regression/az_multiple_regression.py
+++ 02-Regression/02-Multiple_Linear_Regression/az_multiple_regression.py
@@ -41,8 +41,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y,
                                                     test_size = 0.2,
                                                     random_state = 0
                                                     )
-
-
 #train 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
